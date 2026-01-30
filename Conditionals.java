068d43c1f5a10236e8d0be3ae50b8295a39f76a5
SarahKinner <156657511+SarahKinner@users.noreply.github.com>
2026-01-29 17:14:56 -0700

    Wrote out the steps for the conditionals program

--- Conditionals.java
+++ Conditionals.java
@@ -1,3 +1,16 @@
+//Number guessing game
+
 public class Conditionals {
+    //Add variables
+
+    //Randomly generate the number that needs to be guessed
+
+    //Print that says Im thinking of a number between 1-10
+
+    //Ask the user to guess the number
+
+    //User inputs the number they guess
+
+    //If, else, else statment if being too high, first else being two low, and second else being the correct number
     
 }
