1ef7e9eff64de048367b45f782217f98c7c99a59
SarahKinner <156657511+SarahKinner@users.noreply.github.com>
2026-01-30 00:48:50 -0700

    way of how the character will attack

--- Classes_Functions.java
+++ Classes_Functions.java
@@ -6,11 +6,21 @@ public class Character {
     int health;
     int attackPower;
 
+    //Constructor needed for the character when making it
     public Character(String name, int health, int attackPower){
         this.name = name;
         this.health = health;
         this.attackPower = attackPower;
     }
+
+    //Shows how character attacking the enemy is going to be handled
+    public void attack(Character target) {
+        target.health -= this.attackPower;
+
+        System.out.println(this.name + "attacks" + target.name + "for" + this.attackPower + "damage!");
+    }
+
+    //
 }
 
 public class Classes_Functions {
