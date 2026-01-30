7dff1f2d2b0d772bd132500334c046044b9a3d23
SarahKinner <156657511+SarahKinner@users.noreply.github.com>
2026-01-29 17:19:59 -0700

    Added a number generator between 1-10

--- Conditionals.java
+++ Conditionals.java
@@ -2,8 +2,11 @@
 
 public class Conditionals {
     //Add variables
+    int SecretNumber = 0;
+    int UserGuess = 0;
 
     //Randomly generate the number that needs to be guessed
+    SecretNumber = (int)(Math.random() * 10) + 1;
 
     //Print that says Im thinking of a number between 1-10
 
