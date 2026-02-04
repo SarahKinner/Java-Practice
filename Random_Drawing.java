9d36978798441d55495653407a546a002592e903
SarahKinner <156657511+SarahKinner@users.noreply.github.com>
2026-02-04 00:57:48 -0700

    Steps steps steps

---
+++ Random_Drawing.java
@@ -0,0 +1,15 @@
+//Random drawing/lottery program
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Random_Drawing {
+    //Make the array list
+
+    //Ask the user to input the names and when they are done
+
+    //Read the users input and put it to the list
+
+    //Draw a random winner from the array list
+
+    //Print out the winner
+}
